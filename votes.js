622ed07c477dcd50a82bd38347cbc97466db8b34
Кто, Ты? Да, Я! <115730229+Ashura-Cyber@users.noreply.github.com>
2024-07-26 01:47:23 +0300

    Update votes.js

--- votes.js
+++ votes.js
@@ -1,1 +1,11 @@
+// votes.js
+export const votes = {
+    photo1: 53,
+    photo2: 25
+};
+
+export function vote(photoId) {
+    votes[photoId]++;
+    document.getElementById(`votes-${photoId}`).innerText = `${votes[photoId]} votes`;
+}
 
